fd4f84997f3b2b07ddb49e705f0df4579364bb2c
Ahmed Hamed <ahmedhrayyan@outlook.com>
2020-06-22 02:06:17 +0200

    Don't ignore build in frontend

---
+++ frontend/build/precache-manifest.6f36dd95eaa3216c8b1b77e0519605d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20bced4d38974d49c9fe77a7c1a9d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e07d4684c6283991956",
+    "url": "/static/css/main.70d3eac5.chunk.css"
+  },
+  {
+    "revision": "6124d6c62f8af1ac032e",
+    "url": "/static/js/2.2f5fd377.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.2f5fd377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e07d4684c6283991956",
+    "url": "/static/js/main.c04f6e35.chunk.js"
+  },
+  {
+    "revision": "60949e83de0aeb529ab1",
+    "url": "/static/js/runtime-main.25c7e1d9.js"
+  },
+  {
+    "revision": "4bb2ea723e6601c4cf9b6d6dd93016c2",
+    "url": "/static/media/alert.4bb2ea72.svg"
+  },
+  {
+    "revision": "a5a584e6d50d67ef005e3f684f25468f",
+    "url": "/static/media/down-arrow.a5a584e6.svg"
+  },
+  {
+    "revision": "ab947aeb992bc370a0a6a7a946f683ec",
+    "url": "/static/media/logo.ab947aeb.svg"
+  },
+  {
+    "revision": "f9e1c18a13221cd7fd907b8b359c0377",
+    "url": "/static/media/question-mark.f9e1c18a.svg"
+  },
+  {
+    "revision": "f206c4cc09911605a4ddd61cf78ec9d5",
+    "url": "/static/media/search.f206c4cc.svg"
+  }
+]);
